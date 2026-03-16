7e841c88916178dd84127599fbf333faa8c0acab
lyszt <kalliddel@gmail.com>
2026-03-16 09:40:34 -0300

    fix: fallsbacks to or true on run so it doesnt give error 222

--- src/dialogue/intro.s
+++ src/dialogue/intro.s
@@ -6,5 +6,5 @@
 .globl intro, intro_reason, intro_reason_2
 defstr intro, "EXTREMIS \n\n This is a weird RPG built in Assembly. Why, you ask? Well..."
 defstr intro_reason, "The universe decided it didn't want me doing anything better.\n\n"
-defstr intro_reason_2, "By the way, in this universe you are a cat.\n You will have uh... skills and stuff and this will be basically a bad copy of Mewgenics.\n \n You ready to play? Y/N \n\n"
+defstr intro_reason_2, " THIS IS THE WORLD OF BANANATOPIA. It's been named by the old high elves which used to go monke and eat banana, but now they don't anymore. They're an advanced civilization. \n \n Come here, follow me. Are you ready? Y/N \n\n"
 
